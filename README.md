57e6027b2fc9e77688b56535fcbf2460341ae776
Petr Tripolsky <tripolskypetr@gmail.com>
2024-08-05 13:00:52 +0500

    react-native-bug

---
+++ README.md
@@ -0,0 +1,34 @@
+# react-native-bug
+
+> The same code published to [Expo Snack by the link](https://snack.expo.dev/@tripolskypetr/react-native-bug)
+
+## Reproduce
+
+1. **Setup enviroment**
+
+Install Android Studio, create Android Emulator, install npm dependencies
+
+```bash
+cd react-native-bug
+npm install
+```
+
+2. **Open in web**
+
+As you can see, there are `Group 1` and `Group 2` with several text inputs
+
+![web](./assets/web.png)
+
+```bash
+npm run web
+```
+
+3. **Launch on android**
+
+Ooops. `Group 2` is missing
+
+![android](./assets/android.png)
+
+```bash
+npm run android
+```
---
+++ react-native-bug/App.js
@@ -0,0 +1,174 @@
+import { ScrollView, Text, View, StyleSheet } from 'react-native';
+
+import { TextInput } from 'react-native-paper';
+
+export const WithBaseline = ({
+  style,
+  children,
+}) => (
+  <View style={[styles.withBaselineRoot, style]}>
+    <View style={styles.withBaseline}>
+      {children}
+    </View>
+    <View style={styles.adjust} />
+  </View>
+);
+
+const NoBaseline = ({
+  style,
+  children,
+}) => (
+  <View style={[styles.noBaselineRoot, style]}>
+    {children}
+  </View>
+);
+
+const FieldWrapper = ({
+  style,
+  children,
+}) => (
+  <View style={[styles.fieldWrapperRoot, style]}>
+    {children}
+  </View>
+);
+
+export default function App() {
+  return (
+    <ScrollView>
+      <NoBaseline>
+        <FieldWrapper style={{ width: '100%', height: 125, justifyContent: 'center' }}>
+          <Text>Group 1</Text>
+        </FieldWrapper>
+        
+        <WithBaseline style={{ width: '100%' }}>
+          <FieldWrapper style={{ width: '25%' }}>
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+          </FieldWrapper>
+          <FieldWrapper style={{ width: '25%' }}>
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+          </FieldWrapper>
+          <FieldWrapper style={{ width: '25%' }}>
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+          </FieldWrapper>
+          <FieldWrapper style={{ width: '25%' }}>
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+          </FieldWrapper>
+        </WithBaseline>
+        <FieldWrapper style={{ width: '100%', height: 125, justifyContent: 'center' }}>
+          <Text>Group 2</Text>
+        </FieldWrapper>
+        <WithBaseline style={{ width: '100%' }}>
+          <FieldWrapper style={{ width: '33%' }}>
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+          </FieldWrapper>
+          <FieldWrapper style={{ width: '33%' }}>
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+          </FieldWrapper>
+          <FieldWrapper style={{ width: '33%' }}>
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+            <TextInput
+              label="Email"
+            />
+          </FieldWrapper>
+        </WithBaseline>
+      </NoBaseline>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  withBaselineRoot: {
+    flexDirection: 'column',
+  },
+  noBaselineRoot: {
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  fieldWrapperRoot: {
+    flexDirection: 'column',
+  },
+  withBaseline: {
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  adjust: {
+    width: '100%',
+    flex: 1,
+  },
+});
